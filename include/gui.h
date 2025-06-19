69b3af1b750b2ab6fda3bc7a0ebc25bf54a17a6b
andrieljose <andrieldata@proton.me>
2025-06-19 14:42:15 -0300

    refactor(gui): modularize gui.h and add page switching support
    - Added new includes for page components: gui_page_generator,
    gui_page_settings, gui_page_vault, and gui_footer
    - Declared show_main_screen() and switch_page() functions in gui.h for
    better page management
    - Improved file structure by centralizing header dependencies and
    providing a cleaner API for GUI navigation

--- include/gui.h
+++ include/gui.h
@@ -1,9 +1,20 @@
 #pragma once
-// header umbrella
+#include <gtk/gtk.h>
+
 #include "gui_create_account.h"
+#include "gui_footer.h"
 #include "gui_globals.h"
 #include "gui_home.h"
 #include "gui_login.h"
 #include "gui_main_window.h"
+#include "gui_page_generator.h"
+#include "gui_page_settings.h"
+#include "gui_page_vault.h"
 #include "gui_utils.h"
-#include <gtk/gtk.h>
+
+#include "generator.h"
+#include "vault.h"
+
+// Tela principal
+void show_main_screen();
+void switch_page(const char *page_name);
