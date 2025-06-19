d8a387e7285f9c98da9ae88b29912d0113224058
andrieljose <andrieldata@proton.me>
2025-06-19 14:43:05 -0300

    refactor(gui): remove deprecated gui_generator header
    - Deleted gui_generator.h as it was replaced by the new modular
    page-based system.
    - Related functionality moved to gui_page_generator.h and implementation
    files.

--- include/gui_generator.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <gtk/gtk.h>
-
-void show_password_generator(GtkButton *button, gpointer user_data);
-void on_generate_password(GtkButton *button, gpointer user_data);
-void on_copy_password(GtkButton *button, gpointer user_data);
