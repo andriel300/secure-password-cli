73d61a62dc232fe015f97a84548a04dde64ddcd2
andrieljose <andrieldata@proton.me>
2025-06-19 11:58:38 -0300

    feat(gui): add create account screen components
    - Created gui_create_account.h
    - Added function declarations for:
      - show_create_account_screen
      - on_create_account_confirm
      - on_back_to_home
    - Part of GUI modularization process

---
+++ include/gui_create_account.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <gtk/gtk.h>
+
+void show_create_account_screen(void);
+void on_create_account_confirm(GtkButton *button, gpointer user_data);
+void on_back_to_home(GtkButton *button, gpointer user_data);
