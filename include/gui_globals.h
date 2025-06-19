a0eff26ff4ce3ca55de8a9be48887a604b0f0066
andrieljose <andrieldata@proton.me>
2025-06-19 11:59:49 -0300

    feat(gui): add global GUI state management
    - Created gui_globals.h to centralize GTK widgets and vault data
    - Added global declarations for:
      - Main containers (main_container, treeview)
      - Windows and input fields (window, entry_username, entry_password,
    etc.)
      - Current vault data and credentials (current_vault,
    current_vault_file, current_password)
    - This improves state management between different GUI screens

---
+++ include/gui_globals.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "vault.h"
+#include <gtk/gtk.h>
+
+// Containers
+extern GtkWidget *main_container;
+extern GtkWidget *treeview;
+
+// Janelas
+extern GtkWidget *window;
+
+// Entradas
+extern GtkWidget *entry_username;
+extern GtkWidget *entry_password;
+extern GtkWidget *entry_confirm;
+extern GtkWidget *entry_login_username;
+extern GtkWidget *entry_login_password;
+
+// Dados do vault
+extern VaultData current_vault;
+extern char current_vault_file[256];
+extern char current_password[256];
