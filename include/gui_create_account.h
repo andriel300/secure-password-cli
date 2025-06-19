70e0a776ecbcc2346c326adf9af2768479d9c365
andrieljose <andrieldata@proton.me>
2025-06-19 14:42:31 -0300

    refactor(gui_create_account): remove unnecessary void keyword in function declaration
    - Simplified function declaration by removing redundant 'void' in
    show_create_account_screen().
    - Minor header cleanup for better readability.

--- include/gui_create_account.h
+++ include/gui_create_account.h
@@ -1,7 +1,5 @@
 #pragma once
-
 #include <gtk/gtk.h>
 
-void show_create_account_screen(void);
+void show_create_account_screen();
 void on_create_account_confirm(GtkButton *button, gpointer user_data);
-void on_back_to_home(GtkButton *button, gpointer user_data);
