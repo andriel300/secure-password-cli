2df5d1e35e6d5b171528e055ce8516bba107cf7e
andrieljose <andrieldata@proton.me>
2025-06-19 14:43:38 -0300

    style(gui_login): remove unnecessary void keyword from function declaration
    - Simplify `show_login_screen` function signature by removing redundant
    `void`.
    - Code style improvement for consistency across header declarations.

--- include/gui_login.h
+++ include/gui_login.h
@@ -1,7 +1,5 @@
 #pragma once
-
 #include <gtk/gtk.h>
 
-void show_login_screen(void);
+void show_login_screen();
 void on_login_confirm(GtkButton *button, gpointer user_data);
-void on_back_to_home_from_login(GtkButton *button, gpointer user_data);
