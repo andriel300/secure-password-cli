78f446e6ad57c981638b889b58780677d0a67c0c
andrieljose <andrieldata@proton.me>
2025-06-19 11:58:19 -0300

    refactor(gui): modularize gui.h into multiple header files
    - Replaced header guard with #pragma once
    - Split gui.h into modular headers:
      - gui_create_account.h
      - gui_globals.h
      - gui_home.h
      - gui_login.h
      - gui_main_window.h
      - gui_utils.h
    - Improved code organization and readability

--- include/gui.h
+++ include/gui.h
@@ -1,22 +1,9 @@
-#ifndef GUI_H
-#define GUI_H
-
+#pragma once
+// header umbrella
+#include "gui_create_account.h"
+#include "gui_globals.h"
+#include "gui_home.h"
+#include "gui_login.h"
+#include "gui_main_window.h"
+#include "gui_utils.h"
 #include <gtk/gtk.h>
-
-void start_gui(int argc, char *argv[]);
-
-// Criação de conta
-void show_create_account_screen();
-void on_create_account_confirm(GtkButton *button, gpointer user_data);
-void on_back_to_home(GtkButton *button, gpointer user_data);
-
-// Login
-void show_login_screen();
-void on_login_confirm(GtkButton *button, gpointer user_data);
-void on_back_to_home_from_login(GtkButton *button, gpointer user_data);
-
-// Tela inicial
-void on_create_account_clicked(GtkButton *button, gpointer user_data);
-void on_unlock_vault_clicked(GtkButton *button, gpointer user_data);
-
-#endif
